c03ce82e2fd7d07056c2d60300908d7c83a0e090
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-04-03 14:36:26 -0500

    Slight update to make sure commits are linked still

--- CSharp/HelloWorld/Form1.Designer.cs
+++ CSharp/HelloWorld/Form1.Designer.cs
@@ -81,7 +81,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 475);
+            this.ClientSize = new System.Drawing.Size(792, 458);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
