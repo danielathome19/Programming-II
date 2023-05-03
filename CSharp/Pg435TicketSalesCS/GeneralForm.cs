4cb71274fe0cc511b0c629fb39320abc6a1c6bdb
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-05-03 08:31:16 -0500

    Update 5/3

---
+++ CSharp/Pg435TicketSalesCS/GeneralForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pg435TicketSalesCS
+{
+    public partial class GeneralForm : Form
+    {
+        private Form myParent;
+        public GeneralForm(Form prt)
+        {
+            InitializeComponent();
+            this.myParent = prt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.myParent.Show();
+            this.Close();
+        }
+
+        decimal decTAXRATE = 0.06m;  // Sales Tax Rate
+        private decimal CalcTax(decimal cost)
+        {
+            // Returns the sales tax on ticket sales
+            return cost * decTAXRATE;
+        }
+
+        private void GeneralForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.myParent.Show();
+        }
+    }
+}
---
+++ CSharp/Pg435TicketSalesCS/StudentForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Pg435TicketSalesCS
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 111);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 474);
+            this.Controls.Add(this.button1);
+            this.Name = "StudentForm";
+            this.Text = "StudentForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StudentForm_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
