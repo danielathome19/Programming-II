26634a58f130b64d859e60532f1dbfe92548a1ae
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-01-22 11:11:15 -0600

    Removed old semester

--- prog52a.py
+++
@@ -1,8 +0,0 @@
-# Lang52a - Area and Perimeter of Rectangle
-length = int(input("Enter length: "))
-width = int(input("Enter width: "))
-area = length * width
-perim = 2 * length + 2 * width
-
-print("Area =", area)
-print("Perimeter = " + str(perim))
