e1a687fb912d6e239137062ccab250b1067ce95d
danielathome19 <dszelogowski@gmail.com>
2024-01-29 20:02:52 +0000

    Update 1/29

---
+++ lp4-1.py
@@ -0,0 +1,19 @@
+copies = int(input("Enter # of copies to be printed: "))
+
+price = 0.0
+# if copies > 0 and copies <= 99:
+if 0 < copies <= 99:
+  price = 0.30
+elif 99 < copies <= 499:
+  price = 0.28
+elif 499 < copies <= 749:
+  price = 0.27
+elif 749 < copies <= 1000:
+  price = 0.26
+elif copies > 1000:
+  price = 0.25
+else:
+  print("The # of copies is invalid!")
+
+print("Price per copy is: $" + str(price))
+print("Total cost is: $" + str(price*copies))
