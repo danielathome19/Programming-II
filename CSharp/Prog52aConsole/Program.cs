5534ded5c8f5115c9c269768ab03c35e7acb6049
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-04-10 14:33:05 -0500

    Update 4/10

---
+++ CSharp/Prog52aConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog52aConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length: ");
+            int len = int.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            int wid = int.Parse(Console.ReadLine());
+            int area = len * wid;
+            int perim = 2 * len + 2 * wid;
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perim);
+            Console.ReadKey();
+        }
+    }
+}
