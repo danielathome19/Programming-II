b4e4fa08ef760f4c2bfca0f8e6eee9cc54e19bce
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-04-15 14:18:35 -0500

    Update 4/15

---
+++ CSharp/Prog54a/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Prog54a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.lblGallons = new System.Windows.Forms.Label();
+            this.lblMpg = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 83);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Car:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DarkGreen;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(12, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 83);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Miles:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkGreen;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(12, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 83);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gallons:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.DarkGreen;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gold;
+            this.label4.Location = new System.Drawing.Point(12, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 83);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MPG:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.BackColor = System.Drawing.Color.PaleGreen;
+            this.lblMiles.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiles.Location = new System.Drawing.Point(232, 117);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(402, 83);
+            this.lblMiles.TabIndex = 4;
+            this.lblMiles.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGallons
+            // 
+            this.lblGallons.BackColor = System.Drawing.Color.PaleGreen;
+            this.lblGallons.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGallons.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGallons.Location = new System.Drawing.Point(232, 225);
+            this.lblGallons.Name = "lblGallons";
+            this.lblGallons.Size = new System.Drawing.Size(402, 83);
+            this.lblGallons.TabIndex = 5;
+            this.lblGallons.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMpg
+            // 
+            this.lblMpg.BackColor = System.Drawing.Color.PaleGreen;
+            this.lblMpg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMpg.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMpg.Location = new System.Drawing.Point(232, 335);
+            this.lblMpg.Name = "lblMpg";
+            this.lblMpg.Size = new System.Drawing.Size(402, 83);
+            this.lblMpg.TabIndex = 6;
+            this.lblMpg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 101);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(232, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 101);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(449, 444);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 101);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1970 VW Bug",
+            "1979 Firebird",
+            "1980 Subaru",
+            "1975 Cutlass"});
+            this.comboBox1.Location = new System.Drawing.Point(232, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(402, 54);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(649, 557);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMpg);
+            this.Controls.Add(this.lblGallons);
+            this.Controls.Add(this.lblMiles);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.Label lblGallons;
+        private System.Windows.Forms.Label lblMpg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
