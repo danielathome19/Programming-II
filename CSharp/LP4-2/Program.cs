d2a40f4fcf5eb8218c8ea6a795bb04dad21e6bc0
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-04-23 14:31:57 -0500

    Update 4/23

---
+++ CSharp/LP4-2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LP4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter weight: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.Write("Enter length: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            int volume = length * width * height;
+
+            // && AND, || OR, ! NOT
+            if (weight > 27 && volume > 100_000)
+                Console.WriteLine("Package is too heavy and too large");
+            else if (weight > 27)
+                Console.WriteLine("Package is too heavy");
+            else if (volume > 100_000)
+                Console.WriteLine("Package is too large");
+            else 
+                Console.WriteLine("Package is okay");
+
+            Console.ReadLine();
+        }
+    }
+}
