86b2d9e9e657e329dd38d36f178bf44d90c6375e
Daniel James Szelogowski <dszelogowski@gmail.com>
2025-01-27 15:03:58 -0600

    Update 1/27

--- prog122c.py
+++
@@ -1,8 +0,0 @@
-# range(stop)               [0, 9)
-# range(start, stop)        [start, stop)
-# range(start, stop, step)  [start, stop)
-for lcv in range(2, 11, 2):
-  col2 = lcv + 1
-  col3 = lcv * 2
-  col4 = lcv**2
-  print(f"{lcv}\t{col2}\t{col3}\t{col4}")
--- prog52a.py
+++
@@ -1,9 +0,0 @@
-# Lang52a - Area and Perimeter of Rectangle
-length = int(input("Enter length: "))
-width = int(input("Enter width: "))
-area = length * width
-perim = 2 * length + 2 * width
-
-print("Area =", area)
-print("Perimeter = " + str(perim))
-# Run with "python FILENAME.py" in the Shell
