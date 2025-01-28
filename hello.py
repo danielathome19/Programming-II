fa2df0c5ca3fdf7304395ad5e3c8d63f0e740f92
Daniel J. Szelogowski <24834480+danielathome19@users.noreply.github.com>
2025-01-28 21:06:43 +0000

    Update 1/28

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
 print("Hello, world!")
+# This is a single-line comment
+""" Data Types:
+int   - Integer (whole number)
+float - Floating-point Number (number w/ a decimal)
+str   - String (any text, in quotes)
+chr   - Character (single keyboard character, in single-quotes)
+bool  - Boolean (either True or False, only)
+"""
+
+""" Arithmetic Operators:
++   Add
+-   Subtract
+*   Multiply
+/   Divide
+//  Floor Divide (Divide and round-down to nearest int)
+%   Mod (Modulus/Remainder from Int division)
+**  Pow
+"""
+
+""" Assignment Operators:
+= Value assignment (x = y)
++= Add to (x += y is shorthand for x = x + y)
+-= Subtract from (shorthand for x = x - y)
+*=
+/=
+//=
+%=
+**=
+"""
